bf702d314b2d9a15f2d97ed37816c733d544e06b
kailash9712s <rabarikailash721@gmail.com>
2025-07-08 16:13:55 +0530

    fix :- className override

--- src/Component/GetInTouch/GetInToch.jsx
+++ src/Component/GetInTouch/GetInToch.jsx
@@ -2,7 +2,7 @@ import './GetInTouch.css';
 
 export default function TouchWithUse() {
     return (
-        <div className="contact-section">
+        <div className="contact-section1">
             <h2 className="contact-heading">Have a Project in Mind?</h2>
             <p className="contact-subheading">
                 Let's discuss how Sat Shri Steel can bring your vision to life with our expertise in steel construction.
